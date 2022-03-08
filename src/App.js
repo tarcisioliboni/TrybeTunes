e9bc2e00649472d31788d06ed70db2b5e57aff67
Tarcisio Liboni <tarcisioliboni@pop-os.localdomain>
2022-03-08 11:51:47 -0300

    Request 01 OK

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import ProfileEdit from './pages/ProfileEdit';
+import Album from './pages/Album';
+import Search from './pages/Search';
+import Favorites from './pages/Favorites';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
     return (
-      <p>
-        TrybeTunes
-      </p>);
+      <>
+        <p>TrybeTunes</p>
+        <BrowserRouter>
+          <Switch>
+            <Route path="/profile/edit" component={ ProfileEdit } />
+            <Route path="/album/:id" component={ Album } />
+            <Route path="/search" component={ Search } />
+            <Route path="/favorites" component={ Favorites } />
+            <Route path="/profile" component={ Profile } />
+            <Route exact path="/" component={ Login } />
+            <Route path="*" component={ NotFound } />
+          </Switch>
+        </BrowserRouter>
+      </>
+    );
   }
 }
 
